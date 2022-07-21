5d2b771ff05933846930d776abe4046b07499294
zhengshugan <zhengshugan@foxmail.com>
2022-07-21 17:11:38 +0800

    feat: add component Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+<div class='footer'>
+  <div>Vue3 Blog @2022 Create by ColdCoffee</div>
+</div>
+</template>
+
+<script setup lang='ts'>
+const msg: string = 'Hello World'
+</script>
+
+<style scoped>
+.footer {
+  text-align: center;
+  padding: 20px;
+  font-weight: bold;
+}
+</style>
