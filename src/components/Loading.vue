ebfba7a8a606d61b4cc2c9cfe9918db80d2261f8
zhengshugan <zhengshugan@foxmail.com>
2022-07-21 17:15:50 +0800

    feat: add component Loading

---
+++ src/components/Loading.vue
@@ -0,0 +1,19 @@
+<template>
+<div class='loading'>
+  <img
+    src='../assets/loading.png'
+    alt='拼命加载中...'
+    />
+</div>
+</template>
+
+<script lang='ts'>
+
+</script>
+
+<style scoped>
+.loading {
+  text-align: center;
+  padding: 30px;
+}
+</style>
