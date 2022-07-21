8f05b15b47293a9d49d54f20fa2fe3186a0c80c8
zhengshugan <zhengshugan@foxmail.com>
2022-07-21 17:14:58 +0800

    feat: add component LoadEnd

---
+++ src/components/LoadEnd.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='load-end'>
+    ---------- 我也是有底线的 ----------
+  </div>
+</template>
+
+<script lang='ts'>
+
+</script>
+
+<style scoped>
+.load-end {
+  text-align: center;
+  padding: 30px;
+}
+</style>
